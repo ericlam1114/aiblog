f956955a3273b8afc52ceadec25f86e99dc5e1dc
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-25 14:20:50 -0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,17 +12,17 @@ function Blog({ posts }) {
     <>
       <Header />
       <Hero />
-      <ul className="flex flex-wrap mt-10 justify-center space-x-8 ">
+      <ul className="flex flex-wrap mt-10 justify-center space-x-8">
         {posts.map((post) => (
           <li key={post.slug} className="w-1/3 text-orange-500">
             <Link href="/[slug]" as={`/${post.slug}`}>
-              <div className="bg-yellow-100 rounded-lg shadow-lg p-4 hover:scale-95 duration-300 transition">
+              <div className="bg-yellow-100 rounded-lg shadow-lg p-4 hover:scale-95 duration-300 transition ">
                 <h2 className="text-2xl font-bold mb-2">{post.title}</h2>
                 <p className="mb-2 text-gray-700">{post.date}</p>
                 <Image
                   src={"/../public/images/" + `${post.slug.toLowerCase()}` + ".png"}
                   alt={post.title}
-                  className="w-full h-48 rounded-lg object-cover"
+                  className="w-full h-48 rounded-lg object-scale-down"
                   width={400}
                   height={100}
                 />
