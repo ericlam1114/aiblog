a2e3bd184f6cb6771dc6b906c7d3dad04e1c42cf
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-25 11:43:01 -0800

    nest fix

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -37,7 +37,7 @@ const BlogPost = ({ post }) => {
     return <div>Loading...</div>;
   }
 
-  return <ReactMarkdown children={post.content} />;
+  return <ReactMarkdown>{post.content}</ReactMarkdown>;
 };
 
 export default BlogPost;
