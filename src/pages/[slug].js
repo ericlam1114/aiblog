807400cad86088a738e35bd69ea812fb8b5d5dac
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-25 11:39:18 -0800

    blog data complete

---
+++ src/pages/[slug].js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import matter from "gray-matter";
+import fs from "fs";
+import path from "path";
+import ReactMarkdown from "react-markdown";
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const filePath = path.join(process.cwd(), "posts", `${slug}.md`);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const data = matter(fileContent);
+
+  return {
+    props: {
+      post: { ...data, content: data.content, orig: data.orig.toString() },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const postsDirectory = path.join(process.cwd(), "posts");
+  const filenames = fs.readdirSync(postsDirectory);
+  const paths = filenames.map((filename) => ({
+    params: { slug: filename.replace(".md", "") },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const BlogPost = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return <ReactMarkdown children={post.content} />;
+};
+
+export default BlogPost;
